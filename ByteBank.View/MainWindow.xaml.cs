7def0efe7b50a5bf66c0ffe7f892fc1419d43a5b
Icaro Caires <icaro.caires@interplayers.com.br>
2021-06-02 20:22:41 -0300

    Capitulo 3

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -19,177 +19,202 @@ using System.Windows.Shapes;
 
 namespace ByteBank.View
 {
-    public partial class MainWindow : Window
-    {
-        private readonly ContaClienteRepository r_Repositorio;
-        private readonly ContaClienteService r_Servico;
+	public partial class MainWindow : Window
+	{
+		private readonly ContaClienteRepository r_Repositorio;
+		private readonly ContaClienteService r_Servico;
 
-        public MainWindow()
-        {
-            InitializeComponent();
-
-            r_Repositorio = new ContaClienteRepository();
-            r_Servico = new ContaClienteService();
-        }
-
-        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
-        {
-          
-            var inicio = DateTime.Now;
-            var contas = r_Repositorio.GetContaClientes();
-
-            //var resultado = ResolvendoComThreads(contas);
-
-            //Reativação do botão dentro do metodo ResolvendoComTasks
-            BtnProcessar.IsEnabled = false;
-            var resultado = ResolvendoComTasks(contas);
-
-
-            //comentado para ser executado dentro no metodo ResolvendoComTasks no WhenAll desta forma não travando a tela principal
-            //Descomentar em caso de alteração
-            //var fim = DateTime.Now;
-            //AtualizarView(resultado, fim - inicio);
-
-           
-        }
-
-        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
-        {
-            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
-            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
-
-            LstResultados.ItemsSource = result;
-            TxtTempo.Text = mensagem;
-        }
-
-        private List<string> ResolvendoComTasksMelhorado(IEnumerable<ContaCliente> contas)
-        {
-            //TaskScheduler da interface grafica
-            var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
-            var inicio = DateTime.Now;
-            var resultado = new List<string>();
-
-            var contasTarefas = contas.Select(conta =>
-            {
-                return Task.Factory.StartNew(() =>
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                });
-            }).ToArray();
-
-
-            //Para a tarefa principal para esperar o termino de todas as tasks passadas
-            //Task.WaitAll(contasTarefas);
-
-            //Retorna uma task para ser executada e não parar a tarefa principal
-
-            Task.WhenAll(contasTarefas)
-                .ContinueWith(task => {
-                    var fim = DateTime.Now;
-                    AtualizarView(resultado, fim - inicio);
-                }, taskSchedulerUI)
-                    .ContinueWith(task => {
-                        BtnProcessar.IsEnabled = true;
-                    }, taskSchedulerUI); //diz para essa tarefa rodar no Scheduler da interface grafica
-
-
-            return resultado;
-        }
-
-        private List<string> ResolvendoComTasks(IEnumerable<ContaCliente> contas)
+		public MainWindow()
 		{
-            //TaskScheduler da interface grafica
-           var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
-            var inicio = DateTime.Now;
-            var resultado = new List<string>();
+			InitializeComponent();
 
-            var contasTarefas = contas.Select(conta =>
-            {
-                return Task.Factory.StartNew(() =>
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                });
-            }).ToArray();
-
-
-            //Para a tarefa principal para esperar o termino de todas as tasks passadas
-            //Task.WaitAll(contasTarefas);
-
-            //Retorna uma task para ser executada e não parar a tarefa principal
-           
-            Task.WhenAll(contasTarefas)
-                .ContinueWith(task =>{
-                    var fim = DateTime.Now;
-                    AtualizarView(resultado, fim - inicio);
-                }, taskSchedulerUI)
-                    .ContinueWith(task => {
-                        BtnProcessar.IsEnabled = true;
-                    }, taskSchedulerUI); //diz para essa tarefa rodar no Scheduler da interface grafica
-
-
-            return resultado;
+			r_Repositorio = new ContaClienteRepository();
+			r_Servico = new ContaClienteService();
 		}
 
-        private List<string> ResolvendoComThreads(IEnumerable<ContaCliente> contas)
+		private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
 		{
-            var contasQuantidadePorThread = contas.Count() / 4;
-            var contas_parte1 = contas.Take(contasQuantidadePorThread);
-            var contas_parte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
-            var contas_parte3 = contas.Skip(contasQuantidadePorThread * 2).Take(contasQuantidadePorThread);
-            var contas_parte4 = contas.Skip(contasQuantidadePorThread * 3);
+			var contas = r_Repositorio.GetContaClientes();
 
-            var resultado = new List<string>();
+			//Reativação do botão dentro do metodo ResolvendoComTasks
+			BtnProcessar.IsEnabled = false;
+			//ResolucaoComThreads(contas);
+			//ResolucaoComTasks(contas);
+			//ResolucaoComTasksMelhorado(contas);
+			ResolucaoComAsyncAwait(contas);
 
-            AtualizarView(new List<string>(), TimeSpan.Zero);
+		}
 
-            Thread thread_parte1 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte1)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte2 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte2)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte3 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte3)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte4 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte4)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
+		private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+		{
+			var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+			var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
 
-            thread_parte1.Start();
-            thread_parte2.Start();
-            thread_parte3.Start();
-            thread_parte4.Start();
+			LstResultados.ItemsSource = result;
+			TxtTempo.Text = mensagem;
+		}
 
-            while (thread_parte1.IsAlive || thread_parte2.IsAlive
-                || thread_parte3.IsAlive || thread_parte4.IsAlive)
-            {
-                Thread.Sleep(250);
-                //Não vou fazer nada
-            }
+		private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
+		{
+			var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+			var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
 
-            return resultado;
-        }
-    }
+			LstResultados.ItemsSource = result;
+			TxtTempo.Text = mensagem;
+		}
+
+		private async void ResolucaoComAsyncAwait(IEnumerable<ContaCliente> contas)
+		{
+			var inicio = DateTime.Now;
+
+			var resultado = await ConsolidarContas(contas);
+
+			var fim = DateTime.Now;
+			AtualizarView(resultado, fim - inicio);
+			BtnProcessar.IsEnabled = true;
+		}
+
+		private void ResolucaoComTasksMelhorado(IEnumerable<ContaCliente> contas)
+		{
+			var inicio = DateTime.Now;
+			//TaskScheduler da interface grafica
+			var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
+
+			ConsolidarContas(contas)
+				.ContinueWith(task =>
+				{
+					var fim = DateTime.Now;
+					var resultado = task.Result;
+					AtualizarView(resultado, fim - inicio);
+				}, taskSchedulerUI)
+					.ContinueWith(task =>
+					{
+						BtnProcessar.IsEnabled = true;
+					}, taskSchedulerUI); //diz para essa tarefa rodar no Scheduler da interface grafica
+		}
+
+		private async Task<string[]> ConsolidarContasMelhorado(IEnumerable<ContaCliente> contas)
+		{
+			var tasks = contas.Select(conta =>
+				Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(conta))
+			);
+
+			return await Task.WhenAll(tasks);
+		}
+
+		private Task<List<string>> ConsolidarContas(IEnumerable<ContaCliente> contas)
+		{
+			var resultado = new List<string>();
+
+			var tasks = contas.Select(conta =>
+			{
+				return Task.Factory.StartNew(() =>
+				{
+					var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+					resultado.Add(resultadoConta);
+				});
+			});
+
+			return Task.WhenAll(tasks).ContinueWith(t =>
+		   {
+			   return resultado;
+		   });
+		}
+
+		private void ResolucaoComTasks(IEnumerable<ContaCliente> contas)
+		{
+			//TaskScheduler da interface grafica
+			var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
+			var inicio = DateTime.Now;
+			var resultado = new List<string>();
+
+			var contasTarefas = contas.Select(conta =>
+			{
+				return Task.Factory.StartNew(() =>
+				{
+					var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+					resultado.Add(resultadoConta);
+				});
+			}).ToArray();
+
+
+			//Para a tarefa principal para esperar o termino de todas as tasks passadas
+			//Task.WaitAll(contasTarefas);
+
+			//Retorna uma task para ser executada e não parar a tarefa principal
+
+			Task.WhenAll(contasTarefas)
+				.ContinueWith(task =>
+				{
+					var fim = DateTime.Now;
+					AtualizarView(resultado, fim - inicio);
+				}, taskSchedulerUI)
+					.ContinueWith(task =>
+					{
+						BtnProcessar.IsEnabled = true;
+					}, taskSchedulerUI); //diz para essa tarefa rodar no Scheduler da interface grafica
+		}
+
+		private void ResolucaoComThreads(IEnumerable<ContaCliente> contas)
+		{
+			var inicio = DateTime.Now;
+			var contasQuantidadePorThread = contas.Count() / 4;
+			var contas_parte1 = contas.Take(contasQuantidadePorThread);
+			var contas_parte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
+			var contas_parte3 = contas.Skip(contasQuantidadePorThread * 2).Take(contasQuantidadePorThread);
+			var contas_parte4 = contas.Skip(contasQuantidadePorThread * 3);
+
+			var resultado = new List<string>();
+
+			AtualizarView(new List<string>(), TimeSpan.Zero);
+
+			Thread thread_parte1 = new Thread(() =>
+			{
+				foreach (var conta in contas_parte1)
+				{
+					var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+					resultado.Add(resultadoProcessamento);
+				}
+			});
+			Thread thread_parte2 = new Thread(() =>
+			{
+				foreach (var conta in contas_parte2)
+				{
+					var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+					resultado.Add(resultadoProcessamento);
+				}
+			});
+			Thread thread_parte3 = new Thread(() =>
+			{
+				foreach (var conta in contas_parte3)
+				{
+					var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+					resultado.Add(resultadoProcessamento);
+				}
+			});
+			Thread thread_parte4 = new Thread(() =>
+			{
+				foreach (var conta in contas_parte4)
+				{
+					var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+					resultado.Add(resultadoProcessamento);
+				}
+			});
+
+			thread_parte1.Start();
+			thread_parte2.Start();
+			thread_parte3.Start();
+			thread_parte4.Start();
+
+			while (thread_parte1.IsAlive || thread_parte2.IsAlive
+				|| thread_parte3.IsAlive || thread_parte4.IsAlive)
+			{
+				Thread.Sleep(250);
+				//Não vou fazer nada
+			}
+
+			var fim = DateTime.Now;
+			AtualizarView(resultado, fim - inicio);
+		}
+	}
 }
